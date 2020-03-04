b211a7b2262a3adc0cace1ca84d5c0932704c083
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2020-03-04 10:35:33 +0100

    Model save dir (2).

--- models/mod-dummy/build/scripts/train_model.py
+++ models/mod-dummy/build/scripts/train_model.py
@@ -20,16 +20,21 @@ history = model.fit(xs, ys, epochs=500, verbose=0)
 print("Finished training the model!")
 
 SCRIPTS_DIR = PurePath(os.path.dirname(__file__))
+print(SCRIPTS_DIR)
 ROOT_DIR = SCRIPTS_DIR.parents[1]
+print(ROOT_DIR)
 ROOT_DIR_NAME = os.path.basename(ROOT_DIR)
+print(ROOT_DIR_NAME)
 MODEL_DIR = PurePath(ROOT_DIR, 'model')
+print(MODEL_DIR)
 # Model should be saved under model/<model-name> path
 MODEL_SAVE_DIR = os.path.join(MODEL_DIR, ROOT_DIR_NAME)
+print(MODEL_SAVE_DIR)
 os.mkdir(MODEL_SAVE_DIR)
 
 version = 8
 
-export_path = os.path.join(MODEL_DIR, str(version))
+export_path = os.path.join(MODEL_SAVE_DIR, str(version))
 model.save(export_path, save_format="tf")
 
 print('\nexport_path = {}'.format(export_path))
